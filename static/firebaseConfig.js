fc8a051b4ee975f040596cab9be0e4ac5e5ea1d6
Jonathan Ernest <kjernest01@gmail.com>
2024-04-26 09:39:41 -0500

    concat problem

--- static/firebaseConfig.js
+++ static/firebaseConfig.js
@@ -272,7 +272,7 @@ function fetchTimestamps(model) {
                             return xd.json();
                         })
                         .then(data => {
-                            var container = document.getElementById(`${model}-output`);
+                            var container = document.getElementById(`${model}-output-left`);
                             container.style.width = '100%';
                             container.style.height = '100vh';
                             // Handle the data or return it
@@ -344,28 +344,62 @@ function fetchTimestamps(model) {
                 right_option.href = '#';
                 right_option.textContent = timestamp;
                 right_option.addEventListener('click', () => {
-                    fetch('/fetch-output-data/' + model + '/' + option_id)
+                    fetch('/fetch-output-data/' + model + '/' + timestamp)
                         .then(xd => {
                             if (!xd.ok) {
                                 throw new Error('Network response was not ok');
                             }
-                            console.log(xd.json())
+                            // console.log(xd.json())
                             return xd.json();
                         })
                         .then(data => {
+                            var container = document.getElementById(`${model}-output-right`);
+                            container.style.width = '100%';
+                            container.style.height = '100vh';
                             // Handle the data or return it
-                            console.log(data)
-                            console.log("printing value: " + data)
-                            var paragraph = document.createElement("p");
-                            // Set the text content of the <p> element to the data variable
-                            paragraph.textContent = data;
-                            // Get the container where you want to print the content
-                            var container = document.getElementById(`${model}-output`);
-                            // Clear previous content
-                            container.innerHTML = '';
-                            // Append the <p> element to the container
-                            container.appendChild(paragraph);
-                            // return data;
+                            console.log(data);
+                            console.log("printing value: " + data['dataset']);
+
+                            var inputheader = document.createElement("h1");
+                            var inputOutputString = "<b>Inputs: </b>";
+                            inputheader.innerHTML = inputOutputString;
+                            container.appendChild(inputheader);
+
+                            data['input_list'].forEach(input_param => {
+                                var inputoutput = document.createElement("h3");
+                                var dataoutput = document.createElement("p");
+                                inputoutput.innerHTML = input_param + ": ";
+                                dataoutput.innerHTML = data[input_param] + "<br>";
+                                container.appendChild(inputoutput);
+                                container.appendChild(dataoutput);
+                            });
+
+                            var outputheader = document.createElement("h1");
+                            var outputOutputString = "<b>Outputs: </b>";
+                            outputheader.innerHTML = outputOutputString;
+                            container.appendChild(outputheader);
+                            container.scrollTop = container.scrollHeight;
+                            var outputtext = document.createElement("p");
+                            outputtext.innerHTML = data['output'] + "<br>";
+                            container.appendChild(outputtext);
+
+                            Object.keys(data['images']).forEach(function (fileName) {
+                                var fileHeader = document.createElement("h3");
+                                fileHeader.innerHTML = "fileName";
+
+                                // Extract the base64-encoded image data
+                                var imageData = data.images[fileName];
+
+                                // Create an <img> element
+                                var img = new Image();
+
+                                // Set the src attribute to a data URL representing the PNG image data
+                                img.src = 'data:image/png;base64,' + imageData;
+
+                                // Append the image to the container
+                                container.appendChild(img);
+                            });
+
                         })
                         .catch(error => {
                             // Handle errors
